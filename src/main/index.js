d9423e4fde6c17259eb768ee7dcf7680980adc21
7ingout <hadvjin@naver.com>
2022-06-30 17:04:59 +0900

    램프쇼핑몰 보이는 화면 구현 + 상품 받아오기

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './index.scss';
+import axios from 'axios';
 
 const MainPage = (props) => {
+    const [ products, setProducts ] = useState([]);
+    useEffect(()=>{
+        axios.get("http://localhost:3000/products")
+        .then((result)=>{
+            const products = result.data.products;
+            setProducts(products);
+        }).catch((e)=>{
+            console.log(e);
+        })
+    }, [])
+ 
     return (
         <div>
             <div id="main">
@@ -13,78 +25,20 @@ const MainPage = (props) => {
                     <h2>그린조명 최신상품</h2>
                     <div id="product-items">
                         {/* 나중에 map 이용해서 밑에꺼 8개 뿌려줄거임 */}
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
+                        {products.map(product => (
+                            <div className="product-card" key={product.id}>
+                                <div className='product-img'>
+                                    <img src={product.imgsrc} alt="" />
+                                </div>
+                                <div className='product-contents'>
+                                    <span className='product-name'>제품명 {product.name}</span>
+                                    <span className='product-price'>가격 {product.price}</span>
+                                    <div className='product-seller'>
+                                    <img src="images/icons/avatar.png" alt=""/>{product.seller}
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
-                                </div>
-                            </div>
-                        </div>
-                        <div className="product-card">
-                            <div className='product-img'>
-                                <img src="images/products/product1.jpg" alt="" />
-                            </div>
-                            <div className='product-contents'>
-                                <span className='product-name'>제품명</span>
-                                <span className='product-price'>가격</span>
-                                <div className='product-seller'>
-                                    <img src="images/icons/avatar.png" alt=""/>아무나
-                                </div>
-                            </div>
-                        </div>
+                        ))}   
                     </div>
                 </div>
             </div>
